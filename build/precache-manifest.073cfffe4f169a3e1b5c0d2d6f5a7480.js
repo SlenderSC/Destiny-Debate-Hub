64777eb9807a814ed21d08b7d3d33ea90c366887
Oceanhips <oceanhipsmail@gmail.com>
2020-04-12 18:01:19 -0400

    font loading sequence issues?

---
+++ build/precache-manifest.073cfffe4f169a3e1b5c0d2d6f5a7480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "103ce736578960236506828e2f3acf98",
+    "url": "/static/media/Eclipse-1s-200px.103ce736.svg"
+  },
+  {
+    "revision": "03a7f67c33c44897db76fa072725df8d",
+    "url": "/static/media/Alata-Regular.03a7f67c.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ceff479ed4b9e80f96d",
+    "url": "/static/js/main.bcedd2bb.chunk.js"
+  },
+  {
+    "revision": "466a8baf7f0bb843f255",
+    "url": "/static/js/2.49a07e5c.chunk.js"
+  },
+  {
+    "revision": "2ceff479ed4b9e80f96d",
+    "url": "/static/css/main.9b5bb8e0.chunk.css"
+  },
+  {
+    "revision": "bb027d0cb3339a96c06ec49e383b8425",
+    "url": "/index.html"
+  }
+];
